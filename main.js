14d319d96a2d5192efaa084a0583a3ad6c860ca6
reedsans <oladsposanusi@gmail.com>
2025-01-07 16:31:53 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,37 @@
+//
+// //
+// // // VARIABLES
+let filmTitle;
+let filmRating;
+const watchlist = []
+
+//
+// //
+// // //FUNCTIONS
+const promptUser = function(){
+    filmTitle = window.prompt("What movie are you interested in seeing?")
+    filmRating = window.prompt("Enter the imdb rating")
+
+    //
+    // //
+    // // //CONDITIONALS
+    if (filmRating > 5 && !isFinite(filmTitle)){
+        addToWatchlist(filmTitle)
+    } else if (filmRating < 4.5){
+        window.alert("Rating is too low to be added to the watchlist")
+    } else {
+        window.alert("Invalid movie title. Cannot accept only numbers")
+    }
+    document.getElementById('target').innerHTML = `Here's your movie watchlist: ${watchlist}`
+}
+
+const addToWatchlist = function(title){
+    watchlist.push(title)
+}
+
+const addButton = document.getElementById('add-btn')
+addButton.addEventListener('click', promptUser)
+
+
+
+
